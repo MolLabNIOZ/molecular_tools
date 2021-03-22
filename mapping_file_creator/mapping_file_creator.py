59e4daaa92fb87d960fe9fa90479580ca4222e7c
maartje_git <brouwer.mm@gmail.com>
2021-03-22 13:52:06 +0100

    Made it work with primerIDs instead of sampleIDs
    - Added # in front of SampleID in mappingfile header
    - In the new version the user has to supply a column with Fwd primers and a column with Rev primers instead of sampleIDs. SampleIDs will be generated automatically
    - Added NIOZ# to InputFileName, not sure if that is OK for the pipeline, but gives more info about the specific file
    -In final mapping_file add columns, instead of insert
    -Removed some unnecessary columns from the final mapping_file

--- mapping_file_creator/mapping_file_creator.py
+++ mapping_file_creator/mapping_file_creator.py
@@ -1,7 +1,9 @@
 # =============================================================================
+# Title:    Mapping File creator
+# Version:  2.0
 # Author:   Maartje Brouwer
 # Goal:     Automating mapping file generation
-# Date:     210318
+# Date:     210322
 # =============================================================================
 
 ## Uptil now we had to generate mapping files for every sequencing run by hand.
@@ -15,13 +17,17 @@ import pandas as pd       # to be able to work with tables
 from Bio.Seq import Seq   # to be able to do compl_rev
 
 # !!! Set variables for your mappingfile
-sample_file = 'NIOZ197.csv'
-  # Sample_file must be .csv and column 1 must be SampleID
-  # SampleID should be NIOZ#.fwprimer#.rvprimer# --> e.g. NIOZ197.001.252
-  # After SampleID, you can add as many columns of metadata as you like
+sample_file = 'NIOZ184_primerIDs.csv'
+NIOZnumber = 'NIOZ184'
+  # Sample_file must be .csv
+  # $1 and $2 must be Forward_ and Reverse_ primerIDs: 
+  # You can add as many columns of metadata as you like
   # This metadata will be added to the end of the mappingfile
+  # !!! The last column must be Description (sample name or control etc.)
+  # Forward_primer;Reverse_primer;PurifMethod;TargetAmpliconSize;Description
+  # 515F_Golay001;926RBC_Golay252;GelQuant;600bp400bp;1A1P
 fw_primer = '515F'
-rv_primer = '926RBC'    # '806RB' is also available
+rv_primer = '806RB'    # '806RB' and '926RBC' are available
   # For any other primer we need to make a new .csv list
   # primer list must look like this: 3 columns:
   # $1 Forward_primer or Reverse_primer --> primername _Golay### 
@@ -33,84 +39,78 @@ rv_primer = '926RBC'    # '806RB' is also available
 
   
 # Import needed files
-sample_list = pd.read_csv(sample_file, delimiter=';')
+sample_file = pd.read_csv(sample_file, delimiter=';')
 fw_primers = pd.read_csv(fw_primer + '.csv', delimiter=';')
 rv_primers = pd.read_csv(rv_primer + '.csv', delimiter=';')
 
-## Get the primer pair numbers from the sampleID
-# make an empty dataframe with the desired columns
-primerIDs = pd.DataFrame(columns=['SampleID', 'Forward_primer', 'Reverse_primer'])
-# append the sampleIDs from the sample_list
-primerIDs ['SampleID'] = sample_list['SampleID']
-# split fwd and rev primer numbers and add to right column
-primerIDs ['Forward_primer'] = (
-    (sample_list['SampleID'].str.split('.',expand=True))[1].astype(str))
-primerIDs ['Reverse_primer'] = (
-    (sample_list['SampleID'].str.split('.',expand=True))[2].astype(str))
-# add primer names to the primer numbers
-primerIDs ['Forward_primer'] = fw_primer + '_Golay' + primerIDs ['Forward_primer']
-primerIDs ['Reverse_primer'] = rv_primer + '_Golay' + primerIDs ['Reverse_primer']
+## Generate sampleIDs
+# Create a new empty dataframe
+df = pd.DataFrame()
+# Insert primers
+df['Forward_primer']=(sample_file['Forward_primer'])
+df['Reverse_primer']=(sample_file['Reverse_primer'])
+# Extract primernumbers
+df['Forward_primer_number'] = (
+    (sample_file['Forward_primer'].str.split('y',expand=True))[1].astype(str))
+df['Reverse_primer_number'] = (
+    (sample_file['Reverse_primer'].str.split('y',expand=True))[1].astype(str))
+# Generate SampleIDs
+df['#SampleID'] = (
+    NIOZnumber + '.' + 
+    df ['Forward_primer_number'] + '.' + 
+    df ['Reverse_primer_number'])
 
-## Add primer sequence and barcode sequences to primerIDs
-primerIDs = pd.merge(primerIDs, fw_primers)
-primerIDs = pd.merge(primerIDs, rv_primers)
+## Add primer sequence and barcode sequences from database to samples
+df = pd.merge(df, fw_primers)
+df = pd.merge(df, rv_primers)
 
 ## Get complement reverse of reverse primer barcode
 # Make empty new column
-primerIDs ['RevComplReverseBarcodesequence'] = ''
+df ['RevComplReverseBarcodesequence'] = ''
 # For every sample get the rev_compl of the reverse primer barcode
-for sample in primerIDs.index:
-    Barcode_Reverse_Primer = Seq(primerIDs['Barcode_Reverse_Primer'][sample])
+for sample in df.index:
+    Barcode_Reverse_Primer = Seq(df['Barcode_Reverse_Primer'][sample])
     compl_rev_barcode = Barcode_Reverse_Primer.reverse_complement()
     # add reverse complement to the dataframe
-    primerIDs.at[sample,'RevComplReverseBarcodesequence'] = compl_rev_barcode
+    df.at[sample,'RevComplReverseBarcodesequence'] = compl_rev_barcode
 
 ## Construct BarcodeSequence (barcode_fwd + revcompl_barcode_rev)
 # Make empty new column
-primerIDs ['BarcodeSequence'] = ''
+df ['BarcodeSequence'] = ''
 # For every sample make the BarcodeSequence
-for sample in primerIDs.index:
-    BarcodeSequence = ((primerIDs['Barcode_Forward_Primer'][sample]) + 
-                        (primerIDs['RevComplReverseBarcodesequence'][sample]))
+for sample in df.index:
+    BarcodeSequence = ((df['Barcode_Forward_Primer'][sample]) + 
+                        (df['RevComplReverseBarcodesequence'][sample]))
     # add BarcodeSequence to the dataframe
-    primerIDs.at[sample,'BarcodeSequence'] = BarcodeSequence
+    df.at[sample,'BarcodeSequence'] = BarcodeSequence
 
-## Construct InputFileName (full_name_fwd + full_name_rev + .fasta)
+## Construct InputFileName (NIOZnumber.full_name_fwd.full_name_rev.fasta)
 # Make empty new column
-primerIDs ['InputFileName'] = ''
+df ['InputFileName'] = ''
 # For every sample make the InputFileName
-for sample in primerIDs.index:
-    Forward_Name = (primerIDs['Forward_primer'][sample])
-    Reverse_Name = (primerIDs['Reverse_primer'][sample])
-    InputFileName = Forward_Name + '.' + Reverse_Name + '.fasta'
+for sample in df.index:
+    Forward_Name = (df['Forward_primer'][sample])
+    Reverse_Name = (df['Reverse_primer'][sample])
+    InputFileName = (
+        NIOZnumber + '.' + Forward_Name + '.' + Reverse_Name + '.fasta')
     # add BarcodeSequence to the dataframe
-    primerIDs.at[sample,'InputFileName'] = InputFileName
+    df.at[sample,'InputFileName'] = InputFileName
 
 ## Assemble final mappingfile
 # Make an empty dataframe
 mf = pd.DataFrame()
-# Column 1: SampleID
-mf.insert(0, 'SampleID', sample_list['SampleID'])
-# Column 2: BarcodeSequence
-mf.insert(1, 'BarcodeSequence', primerIDs['BarcodeSequence'])
-# Column 3, 4, 5, 6, 7:
-# LinkerPrimerSequence, Barcode_Forward_Primer, Barcode_Reverse_Primer, 
-# ReversePrimer, RevComplReverseBarcodesequence
-mf.insert(2, 'LinkerPrimerSequence', primerIDs['LinkerPrimerSequence'])
-mf.insert(3, 'Barcode_Forward_Primer', primerIDs['Barcode_Forward_Primer'])
-mf.insert(4, 'Barcode_Reverse_Primer', primerIDs['Barcode_Reverse_Primer'])
-mf.insert(5, 'ReversePrimer', primerIDs['ReversePrimer'])
-mf.insert(6, 
-          'RevComplReverseBarcodesequence', 
-          primerIDs['RevComplReverseBarcodesequence'])
-# Column 6: InputFileName
-mf.insert(7, 'InputFileName', primerIDs['InputFileName'])
-# Column 7 and on... extra data from sample_list
-for column in sample_list.columns[1:]:
-    mf[column] = sample_list[column]
+# First column: SampleID
+mf['#SampleID'] = df['#SampleID']
+# Next column: BarcodeSequence
+mf['BarcodeSequence'] = df['BarcodeSequence']
+# Next column: LinkerPrimerSequence
+mf['LinkerPrimerSequence'] = df['LinkerPrimerSequence']
+# Next column: InputFileName
+mf['InputFileName'] = df['InputFileName']
+# After that... extra data from sample_file
+for column in sample_file.columns[2:]:
+    mf[column] = sample_file[column]
 
 ## Save mapping_file
-# Extraxt NIOZ number from SampleID
-RUNID =(sample_list['SampleID'].str.split('.',expand=True))[0].astype(str).values[0]
 # Save file as RUNID_mapping_file.txt, tab delimited and without the index
-mf.to_csv(RUNID + "_mapping_file.txt", sep="\t", index=False)
+mf.to_csv(NIOZnumber + "_mapping_file.txt", sep="\t", index=False)
