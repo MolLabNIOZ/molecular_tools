609242196bf7249afa2f2aff4c770fa4b65d36dc
Maartje <brouwer.mm@gmail.com>
2021-05-28 10:04:08 +0200

    HS or BR also possible
    In the previous version it was only possible to analyze measurements with both HS and BR. With this version it also works when you only used one of the 2 assays.

--- Qubit_on_CFX/qubit_on_CFX.py
+++ Qubit_on_CFX/qubit_on_CFX.py
@@ -1,3 +1,4 @@
+
 # =============================================================================
 # Title:    Qubit_on_CFX analysis tool
 # Version:  1.0
@@ -23,111 +24,159 @@ from matplotlib import pyplot as plt
 
 
 # ==================Import file================================================
-project = "190905_QubitLAZ19-1_to_LAZ19-40"
-csv = '190905-Qubit_LAZ19-1_to_LAZ19-40-End_Point_Results_SYBR.csv'
+# =============================================================================
+project = "210527-QubitLena"
+csv = '//zeus/mmb/molecular_ecology/mollab_team/Projects/2021/2021_Lena/210422-LenaQubit.csv'
 data = pd.read_csv(csv, delimiter=';')
 # =============================================================================
 
 
 # ==================Making a standard curve====================================
+# =============================================================================
 # Extract standard curve data
 stdcurveHSraw = data[(data["Sample"].str.startswith("HS "))]
 stdcurveBRraw = data[(data["Sample"].str.startswith("BR "))]
-# Make empty dataframe
-stdcurveHS = pd.DataFrame()
-stdcurveBR = pd.DataFrame()
-# Cut ng/ul from standard name
-stdcurveHS["ng/ul"] = stdcurveHSraw["Sample"].str.split(' ', expand=True)[1].astype(float)
-stdcurveBR["ng/ul"] = stdcurveBRraw["Sample"].str.split(' ', expand=True)[1].astype(float)
-# Get "End RFU" of the standards
-stdcurveHS["End RFU"] = stdcurveHSraw["End RFU"]
-stdcurveBR["End RFU"] = stdcurveBRraw["End RFU"]
 
-# Linear regression + interpolation for standard curve
-HSslope, HSintercept, rv, pv, se = stats.linregress(stdcurveHS["ng/ul"], stdcurveHS["End RFU"])
-HS_interp = np.linspace(np.min(stdcurveHS["ng/ul"]), np.max(stdcurveHS["ng/ul"]), num=500)
+# ==================High Sensitivity===========================================
+# when there are HS measurements
+if stdcurveHSraw.empty is False: 
+    stdcurveHS = pd.DataFrame()
+    # Cut ng/ul value from standard name
+    stdcurveHS["ng/µl"] = (
+        stdcurveHSraw["Sample"].str.split(' ', expand=True)[1].astype(float))
+        # Expand, returns DataFrame instead of lists of strings
+    # Get "End RFU" of the standards
+    stdcurveHS["End RFU"] = stdcurveHSraw["End RFU"]
+    # Linear regression + interpolation for standard curve
+    HSslope, HSintercept, rv, pv, se = stats.linregress(stdcurveHS["ng/µl"], 
+                                                        stdcurveHS["End RFU"])
+    HS_interp = np.linspace(np.min(stdcurveHS["ng/µl"]), 
+                            np.max(stdcurveHS["ng/µl"]), 
+                            num=500)
+    
+# ==================Broad Range================================================
+# when there are BR measurements
+if stdcurveBRraw.empty is False: 
+    # Make empty dataframe
+    stdcurveBR = pd.DataFrame()
+    # Cut ng/ul value from standard name
+    stdcurveBR["ng/µl"] = (
+        stdcurveBRraw["Sample"].str.split(' ', expand=True)[1].astype(float))
+        # Expand, returns DataFrame instead of lists of strings
+    # Get "End RFU" of the standards
+    stdcurveBR["End RFU"] = stdcurveBRraw["End RFU"]
 
-BRslope, BRintercept, rv, pv, se = stats.linregress(stdcurveBR["ng/ul"], stdcurveBR["End RFU"])
-BR_interp = np.linspace(np.min(stdcurveBR["ng/ul"]), np.max(stdcurveBR["ng/ul"]), num=500)
+    # Linear regression + interpolation for standard curve
+    BRslope, BRintercept, rv, pv, se = stats.linregress(stdcurveBR["ng/µl"], 
+                         stdcurveBR["End RFU"])
+    BR_interp = np.linspace(np.min(stdcurveBR["ng/µl"]), 
+                    np.max(stdcurveBR["ng/µl"]), 
+                    num=500)
 
-# Plot standard curves
-# empty plot
-fig, ax = plt.subplots(ncols=2, dpi=300)
-HSax = ax[0]
-HSax.grid(alpha=0.5)
-HSax.set_title('standardcurve HS', fontsize=16)
-#plot HS curve
-HS = HSax.scatter(stdcurveHS["ng/ul"], stdcurveHS["End RFU"], c='lightsteelblue')
-HSax.set_xlabel('DNA (ng/µl)')
-HSax.set_ylabel('end RFU')
-HSax.plot(
-        HS_interp, 
-        HSintercept + HSslope * HS_interp,
-        label='Linear regression',
-        linestyle='--',
-        c='cornflowerblue'
-        )
+# ==================Plot standard curves=======================================
+# empty plot with 2 columns
+fig, ax = plt.subplots(ncols=2, dpi=300)            
+
+if stdcurveHSraw.empty is False:
+    HSax = ax[0] # HS plot on the left
+    HSax.set_xticks(np.arange(0,25,5)) # x-grid from 0 to 25, with intervals = 5
+    HSax.grid(alpha=0.3) # transparancy of grid 
+    HSax.set_title('standardcurve HS', fontsize=16)
+    # plot HS curve
+    HS = HSax.scatter(stdcurveHS["ng/µl"],      # x-axis
+                      stdcurveHS["End RFU"],    # y-axis
+                      c='lightsteelblue')       # color of the dots
+    HSax.set_xlabel('DNA (ng/µl)')
+    HSax.set_ylabel('end RFU')
+    # draw standard curve line
+    HSax.plot(
+            HS_interp,                          # x-axis
+            HSintercept + HSslope * HS_interp,  # y-axis
+            linestyle='--',                     # style of the line
+            c='cornflowerblue'                  # color of the line
+            )
+    # add equation to plot
+    HSequation = "y = " + str(round(HSslope)) + "X + " + str(round(HSintercept))
+    HSax.text(6,500, HSequation)
+
 # plot BR curve
-BRax = ax[1]
-BRax.grid(alpha=0.5)
-BRax.set_title('standardcurve BR', fontsize=16)
-BR = BRax.scatter(stdcurveBR["ng/ul"], stdcurveBR["End RFU"], c='cornflowerblue')
-BRax.set_xlabel('DNA (ng/µl)')
-BRax.set_ylabel('end RFU')
-BRax.plot(
-        BR_interp, 
-        BRintercept + BRslope * BR_interp,
-        label='Linear regression',
-        linestyle="--",
-        c='blue'
+if stdcurveBRraw.empty is False:
+    BRax = ax[1] # BR plot on the right
+    BRax.set_xticks(np.arange(0,250,50)) # x-grid from 0 to 25, with intervals = 5
+    BRax.grid(alpha=0.3)
+    BRax.set_title('standardcurve BR', fontsize=16)
+    BR = BRax.scatter(stdcurveBR["ng/µl"], 
+                      stdcurveBR["End RFU"], 
+                      c='cornflowerblue')
+    BRax.set_xlabel('DNA (ng/µl)')
+    BRax.set_ylabel('end RFU')
+    # draw standard curve line
+    BRax.plot(
+            BR_interp,                          # x-axis
+            BRintercept + BRslope * BR_interp,  # y-axis
+            linestyle="--",                     # style of the line
+            c='blue'                            # color of the line
+            )
+    # add equation to plot
+    BRequation = (
+        "y = " + str(round(BRslope)) + "X + " + str(round(BRintercept))
         )
+    BRax.text(70,500, BRequation)
+
 # make layout fit better
 plt.tight_layout()
 # save standard curve as .png
 plt.savefig("output/standardcurve"+ project + ".png")
-
 # =============================================================================
 
-
 # ==================Calculating concentrations=================================
-# Make a new dataframe with the HS Samples only
-DNA_concentrations_HS_raw = data.loc[(data["Sample"].str.startswith("HS_"))]
-# Make a new empty dataframe
-DNA_concentrations_HS = pd.DataFrame()
-# Add HS sample names and RFU columns only
-DNA_concentrations_HS["Sample"] = DNA_concentrations_HS_raw["Sample"]
-DNA_concentrations_HS["HS_RFU"] = DNA_concentrations_HS_raw["End RFU"]
-# Add a new column for the concentration
-DNA_concentrations_HS["HS_[DNA] ng/µL"] = ''
-# for every sample calculate concentration ((RFU - y-intercept) / slope)
-for sample in DNA_concentrations_HS.index:
-    concentration = (
-        ((DNA_concentrations_HS['HS_RFU'][sample]) - HSintercept) 
-        / HSslope
-        ) 
-    # add concentration to the dataframe
-    DNA_concentrations_HS.at[sample,'HS_[DNA] ng/µL'] = concentration
+# =============================================================================
+# Make new empty dataframes for HS and BR results
+DNA_concentrations_HS = pd.DataFrame(columns = ["Sample"])
+DNA_concentrations_BR = pd.DataFrame(columns = ["Sample"])
 
-# Make a new dataframe with the BR Samples + RFU only
-DNA_concentrations_BR_raw = data.loc[(data["Sample"].str.startswith("BR_"))]
-DNA_concentrations_BR = pd.DataFrame()
-DNA_concentrations_BR["Sample"] = DNA_concentrations_BR_raw["Sample"]
-DNA_concentrations_BR["BR_RFU"] = DNA_concentrations_BR_raw["End RFU"]
-# calculate concentration ((RFU - y-intercept) / slope)
-DNA_concentrations_BR["BR_[DNA] ng/µL"] = ''
-for sample in DNA_concentrations_BR.index:
-    concentration = (
-        ((DNA_concentrations_BR['BR_RFU'][sample]) - BRintercept) 
-        / BRslope
-        )
-    # add concentration to the dataframe
-    DNA_concentrations_BR.at[sample,'BR_[DNA] ng/µL'] = concentration
+# ==================High Sensitivity===========================================
+if stdcurveHSraw.empty is False:
+    # Get data from HS samples only
+    DNA_concentrations_HS_raw = data.loc[(data["Sample"].str.startswith("HS_"))]
+    # Add HS sample names and RFU columns to results dataframe
+    DNA_concentrations_HS["Sample"] = DNA_concentrations_HS_raw["Sample"]
+    DNA_concentrations_HS["HS_RFU"] = DNA_concentrations_HS_raw["End RFU"]
+    # Add a new column for the concentration
+    DNA_concentrations_HS["HS_[DNA] ng/µL"] = ''
+    # for every sample calculate concentration ((RFU - y-intercept) / slope)
+    for sample in DNA_concentrations_HS.index:
+        concentration = (
+            ((DNA_concentrations_HS['HS_RFU'][sample]) - HSintercept) 
+            / HSslope
+            ) 
+        # add concentration to the dataframe
+        DNA_concentrations_HS.at[sample,'HS_[DNA] ng/µL'] = concentration
+    # Get rid of HS in sample names, to be able to merge samples
+    DNA_concentrations_HS['Sample'] = (
+        (DNA_concentrations_HS['Sample'].str.split('_', 
+        expand=True))[1].astype(str))
 
-# Get rid of HS / BR in sample names, to be able to merge samples
-DNA_concentrations_HS['Sample'] = (
-    (DNA_concentrations_HS['Sample'].str.split('_', expand=True))[1].astype(str))
-DNA_concentrations_BR['Sample'] = (
-    (DNA_concentrations_BR['Sample'].str.split('_', expand=True))[1].astype(str))
+if stdcurveBRraw.empty is False:
+    # Get data from BR samples only
+    DNA_concentrations_BR_raw = data.loc[(data["Sample"].str.startswith("BR_"))]
+    # Add BR sample names and RFU columns to results dataframe    
+    DNA_concentrations_BR["Sample"] = DNA_concentrations_BR_raw["Sample"]
+    DNA_concentrations_BR["BR_RFU"] = DNA_concentrations_BR_raw["End RFU"]
+    # calculate concentration ((RFU - y-intercept) / slope)
+    DNA_concentrations_BR["BR_[DNA] ng/µL"] = ''
+    for sample in DNA_concentrations_BR.index:
+        concentration = (
+            ((DNA_concentrations_BR['BR_RFU'][sample]) - BRintercept) 
+            / BRslope
+            )
+        # add concentration to the dataframe
+        DNA_concentrations_BR.at[sample,'BR_[DNA] ng/µL'] = concentration
+
+    # Get rid of BR in sample names, to be able to merge samples
+    DNA_concentrations_BR['Sample'] = (
+        (DNA_concentrations_BR['Sample'].str.split('_', 
+        expand=True))[1].astype(str))
 
 # Merge HS and BR measurements
 DNA_concentrations = pd.merge(
