9deb22baad7d18eea84c418867ad4099da461f7f
Maartje <78266071+maartje-git@users.noreply.github.com>
2022-02-09 11:39:29 +0100

    automatically extracts primers used from template
    automatically extracts primers used from template
    takes primer info from xlsx file with all primers we have
    
    Works for any primer in this xlsx file...

--- mapping_file_creator/mapping_file_creator.py
+++ mapping_file_creator/mapping_file_creator.py
@@ -1,78 +1,83 @@
-# =============================================================================
-# Title:    Mapping File creator
-# Version:  3.0
-# Author:   Maartje Brouwer
-# Goal:     Automating mapping file generation
-# Date:     210322
-# =============================================================================
+"""
+Automated mapping_file creator
+VERSION: Feb2022
 
-## Uptil now we had to generate mapping files for every sequencing run by hand.
-## Because we always make unique primer-pair combinations for every run, this
-## needs to be done for every run seperately.
-## In excel this is a lot of work, and often goes wrong. Therefore I would like
-## to automate this process.
+If you have different primer sets within 1 sequencing lane
+make seperate mapping_files for the different primer sets
+
+Template can be .xlsx or .csv:
+If .xlsx, sheet with mapping_file data must be called FILL_IN.
+$1 and $2 must be Forward_primer and Reverse_primer
+
+'515F', '926RBC', '806RB', '12S_F1a', '12S_R1',
+'EUKssu_F04' and 'EUKssu_R22mod_A002' are available
+For any other primer we need to make a new .csv list
+Primers should be written like: 
+#515F_Golay001 , 926RBC_Golay002 , 806RB_Golay002 , 12S_F1a_Golay001 , 
+#12S_R1_Golay002 , EUKssu_F04_A001 ,  EUKssu_R22mod_A002
+
+You can add as many columns of metadata as you like
+Add meta data that is equal for all samples to the ReadMe.
+In the FILL_IN only enter metadata that differs between samples
+This metadata will be added to the end of the mappingfile
+The description column will be the last column of the mapping_file
+Make the description as descriptive as possible.
+"""
 
 #### Import needed packages
 import pandas as pd       # to be able to work with dataframes
 from Bio.Seq import Seq   # to be able to do compl_rev
 
+#### !!! Set variables for your mappingfile
+# file_path to mapping_file template (.xlsx or .csv)
+file_path = "\\zeus.nioz.nl\mmb\molecular_ecology\mollab_team\Sequencing\ngs_sequencing\test_template_for_mappingfile_creatorpy.xlsx"
+# Change from windows path to unix path
+file_path = (
+    '/' + file_path.replace('\\', '/').replace('\n', '/n').replace('\t', '/t'))
 
-# !!! Set variables for your mappingfile
-path = '//ZEUS/mmb/molecular_ecology/mollab_team/Sequencing/ngs_sequencing/210616_python_mapping_file_creator/'
-file = path + 'test_template_for_mappingfile_creatorpy.csv'
-NIOZnumber = 'testcsv_NIOZ313'
-  # Sample_file must be .xlsx
-  # Mapping file info must be 
-  # $1 and $2 must be Forward_primer and Reverse_ primer
-  # Primers should be written like: 515F_Golay001 / 926RBC_Golay252
-  # You can add as many columns of metadata as you like
-  # This metadata will be added to the end of the mappingfile
-  # !!! The last column must be Description (sample name or control etc.)
-  # Forward_primer;Reverse_primer;PurifMethod;TargetAmpliconSize;Description
-  # 515F_Golay001;926RBC_Golay252;GelQuant;600bp400bp;1A1P
-fw_primer = '515F'      # '515F' and '12S_F1a'are available
-rv_primer = '926RBC'    # '806RB', '926RBC' and '12S_R1' are available
-  # For any other primer we need to make a new .csv list
-  # primer list must look like this: 3 columns:
-  # $1 Forward_primer or Reverse_primer --> primername _Golay### 
-  # $2 = ForwardPrimer or ReversePrimer --> primer sequence
-  # $3 = LinkerPrimerSequence or Barcode_Reverse_Primer --> barcode sequence
-  # i.e.: 
-  # Reverse_primer;ReversePrimer;Barcode_Reverse_Primer
-  # 926RBC_Golay002;CCGYCAATTYMTTTRAGTTT;CTTCCAACTCAT
+# NIOZ number to name the mapping_file
+NIOZnumber = 'test_mapping'	
+ 
+#### Import needed files
+if file_path.endswith('.xlsx'):
+    sample_file = pd.read_excel(file_path, sheet_name='FILL_IN')
+if file_path.endswith('.csv'):
+    sample_file = pd.read_csv(file_path, delimiter=';')
 
-  
-# Import needed files
-if file.endswith('.xlsx'):
-    sample_file = pd.read_excel(file, sheet_name='FILL_IN')
-if file.endswith('.csv'):
-    sample_file = pd.read_csv(file, delimiter=';')
-
-fw_primers = pd.read_csv(fw_primer + '.csv', delimiter=';')
-rv_primers = pd.read_csv(rv_primer + '.csv', delimiter=';')
-
-## Generate sampleIDs
+#### Generate sampleIDs
 # Create a new empty dataframe
 df = pd.DataFrame()
 # Insert primers
 df['Forward_primer']=(sample_file['Forward_primer'])
 df['Reverse_primer']=(sample_file['Reverse_primer'])
-# Extract primernumbers
+# Extract primernumbers (last 3 characters)
 df['Forward_primer_number'] = (
-    (sample_file['Forward_primer'].str.split('y',expand=True))[1].astype(str))
+    (sample_file['Forward_primer'].str.slice(-3)))
 df['Reverse_primer_number'] = (
-    (sample_file['Reverse_primer'].str.split('y',expand=True))[1].astype(str))
+    (sample_file['Reverse_primer'].str.slice(-3)))
 # Generate SampleIDs
 df['#SampleID'] = (
     NIOZnumber + '.' + 
     df ['Forward_primer_number'] + '.' + 
     df ['Reverse_primer_number'])
 
-## Add primer sequence and barcode sequences from database to samples
+#### Get primer and barcode sequence info
+# Get primer names without barcode number
+fw_primer = sample_file.iloc[1]['Forward_primer'][:-3]
+rv_primer = sample_file.iloc[1]['Reverse_primer'][:-3]
+# Import files with primer
+fw_primers = pd.read_excel(
+    "//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Sequencing/ngs_sequencing/220208_python_mapping_file_creator/primer_lists.xlsx",
+    sheet_name=fw_primer)
+rv_primers = pd.read_excel(
+    "//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Sequencing/ngs_sequencing/220208_python_mapping_file_creator/primer_lists.xlsx",
+    sheet_name=rv_primer)
+
+#### Add primer sequence and barcode sequences from database to samples
 df = pd.merge(df, fw_primers)
 df = pd.merge(df, rv_primers)
 
-## Get complement reverse of reverse primer barcode
+#### Get complement reverse of reverse primer barcode
 # Make empty new column
 df ['RevComplReverseBarcodesequence'] = ''
 # For every sample get the rev_compl of the reverse primer barcode
@@ -82,7 +87,7 @@ for sample in df.index:
     # add reverse complement to the dataframe
     df.at[sample,'RevComplReverseBarcodesequence'] = compl_rev_barcode
 
-## Construct BarcodeSequence (barcode_fwd + revcompl_barcode_rev)
+#### Construct BarcodeSequence (barcode_fwd + revcompl_barcode_rev)
 # Make empty new column
 df ['BarcodeSequence'] = ''
 # For every sample make the BarcodeSequence
@@ -92,7 +97,7 @@ for sample in df.index:
     # add BarcodeSequence to the dataframe
     df.at[sample,'BarcodeSequence'] = BarcodeSequence
 
-## Assemble final mappingfile
+#### Assemble final mappingfile
 # Make an empty dataframe
 mf = pd.DataFrame()
 # First column: SampleID
@@ -110,7 +115,7 @@ mf['ForwardPrimerName'] = df['Forward_primer']
 mf['ReversePrimerName'] = df['Reverse_primer']
 
 
-# After that... extra data from sample_file
+#### Add metadata from sample_file
 for column in sample_file.columns[2:]:
     mf[column] = sample_file[column]
 # Remove everything after the description column
@@ -118,4 +123,5 @@ mf = mf.loc[:,:'description']
 
 ## Save mapping_file
 # Save file as RUNID_mapping_file.txt, tab delimited and without the index
+path = '//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Sequencing/ngs_sequencing/Mapping_files/'
 mf.to_csv(path + NIOZnumber + "_mapping_file.txt", sep="\t", index=False)
