36f3333299289c1a80f53da9274f9d66120048c0
rdebeer21 <rob.de.beer@nioz.nl>
2023-11-09 13:30:56 +0100

    Create from epje to PCR-plate.py

---
+++ transfering of volume/from epje to PCR-plate.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 13:05:07 2023
+
+@author: rdebeer
+"""
+##=============================================================================
+#VARIABLES TO SET!!!
+##=============================================================================
+number_of_samples =                 #enter the number of samples 
+volume_of_sample =                  #enter the volume of the sample
+starting_tip_p20 = ''               #enter the starting tip of the p20 tips
+starting_tip_p200 = ''              #enter the starting tip of the p200 tips
+
+# Are you simulating the protocol, or running it on the OT2?
+simulate = False
+
