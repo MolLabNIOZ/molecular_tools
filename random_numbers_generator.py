3623136c91efb552adddf7d7de18bf6323800a21
Maartje <brouwer.mm@gmail.com>
2024-02-20 10:51:11 +0100

    Update random_numbers_generator.py
    enkel unieke nummers

--- random_numbers_generator.py
+++ random_numbers_generator.py
@@ -5,20 +5,22 @@ Created on Tue Feb 20 10:38:25 2024
 @author: ChatGPT
 """
 
-import random
 
 # Hoeveel getallen heb je?
 totaal = 163
 # Hoeveel random getallen wil je?
 aantal = 10
 
-def generate_random_numbers():
-    # Genereer 10 willekeurige getallen tussen 1 en 163
-    random_numbers = [random.randint(1, totaal) for i in range(aantal)]
-    return random_numbers
 
-# Roep de functie aan om de lijst met willekeurige getallen te verkrijgen
-random_numbers_list = generate_random_numbers()
+import random
+
+def generate_unique_random_numbers():
+    # Genereer 10 unieke willekeurige getallen tussen 1 en 163
+    unique_random_numbers = random.sample(range(1, totaal + 1), aantal)
+    return unique_random_numbers
+
+# Roep de functie aan om de lijst met unieke willekeurige getallen te verkrijgen
+unique_random_numbers_list = generate_unique_random_numbers()
 
 # Druk de resultaten af
-print("Lijst met willekeurige getallen:", random_numbers_list)
+print(f"Lijst met {aantal} unieke willekeurige getallen:", unique_random_numbers_list)
