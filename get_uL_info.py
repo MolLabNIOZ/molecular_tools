9f24392582433bb6543a2d52a1b11fb8007b1768
Maartje <brouwer.mm@gmail.com>
2026-03-06 15:38:32 +0100

    260306

--- get_uL_info.py
+++ get_uL_info.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 ## Import excel sheet
 volume_data = pd.read_excel(
-    "//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Projects/2024/COS/JvG/Roos/NIOZ399_equimolar_pooling_results.xlsx",
-    sheet_name='NIOZ399_equimolar_pooling_resul')
+    "//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Projects/2025/MMB/Helge/Gaia/NIOZ418_equimolar_pooling_results_second_try.xlsx",
+    sheet_name='part 2')
 
 # Make list of volumes (rounded to 2 decimals)
-dna = round(volume_data['50ng'],2).to_list()
+dna = round(volume_data['water_volume'],2).to_list()
 
 # remove NaNs
 dna_noNaNs = [x for x in dna if pd.isnull(x) == False and x != 'nan']
--- reverse_complement/reverse_complement.py
+++ reverse_complement/reverse_complement.py
@@ -17,41 +17,41 @@ from Bio.Seq import Seq   # to be able to do compl_rev
 
 # VARIABLES TO SET#!!!=========================================================
 # =============================================================================
-file_path = 'molecular_tools/reverse_complement/'
-sample_file = file_path + '12S_barcodes_minDistances1_20210916.xlsx'
+file_path = '//zeus.nioz.nl/mmb/molecular_ecology/mollab_team/Projects/2025/MMB/Linda/Celine/'
+sample_file = file_path + 'barcodes.xlsx'
 # =============================================================================
 
 # IMPORTING DATA===============================================================
 # =============================================================================
-file = pd.read_excel(sample_file)
+file = pd.read_excel(sample_file, sheet_name="Sheet1")
 # =============================================================================
 
 # ADD COMPLEMENT REVERSE BARCODES AND CHECK FOR MATCHES========================
 # =============================================================================
 ### New column with reverse complement barcode
 file['reverse_complement']=''
-file['match_name']=''
+# file['match_name']=''
 for primer in file.index:
     barcode = Seq(file['barcode'][primer])
     reverse_complement = barcode.reverse_complement()
     file.at[primer,'reverse_complement'] = str(reverse_complement)
 
 
-    ###Check if barcode matches any RC barcode
-    match = file.index[file['reverse_complement'] == barcode].tolist()
-    if len(match) > 0:
-        file.loc[primer, 'match_name'] = file['Name'][match[0]]
+    # ###Check if barcode matches any RC barcode
+    # match = file.index[file['reverse_complement'] == barcode].tolist()
+    # if len(match) > 0:
+    #     file.loc[primer, 'match_name'] = file['Name'][match[0]]
 
   
-    ###Check if RC barcode matches any barcode
-    match = file.index[file['barcode'] == reverse_complement].tolist()
-    if len(match) > 0:
-        file.loc[primer, 'match_name'] = file['Name'][match[0]]
+    # ###Check if RC barcode matches any barcode
+    # match = file.index[file['barcode'] == reverse_complement].tolist()
+    # if len(match) > 0:
+    #     file.loc[primer, 'match_name'] = file['Name'][match[0]]
 # =============================================================================
 
 # SAVE NEW DATAFRAME===========================================================
 # =============================================================================
-file.to_excel(file_path + '12S_reverse_complement_barcodes.xlsx')
+file.to_excel(file_path + 'rc_barcodes.xlsx')
 # =============================================================================
     
     
